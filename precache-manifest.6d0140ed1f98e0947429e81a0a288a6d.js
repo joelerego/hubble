6b559f0483ccefb0f48b14199b8274b950d14c82
Joel E. Rego <joelerego@gmail.com>
2020-12-28 21:01:12 +0530

    Updates

---
+++ precache-manifest.6d0140ed1f98e0947429e81a0a288a6d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401e244a639b324018860fcac79131be",
+    "url": "/hubble/index.html"
+  },
+  {
+    "revision": "941a077c0236c82047f5",
+    "url": "/hubble/static/js/0.c37f8c90.chunk.js"
+  },
+  {
+    "revision": "d73c04e9a50616bd6cf6",
+    "url": "/hubble/static/js/1.30915e2d.chunk.js"
+  },
+  {
+    "revision": "d9fa6d12fbe8cde1d197",
+    "url": "/hubble/static/js/10.264d8f85.chunk.js"
+  },
+  {
+    "revision": "5610d4614b4b5dd225a0",
+    "url": "/hubble/static/js/11.2d38d476.chunk.js"
+  },
+  {
+    "revision": "c5c2b742b891cd87c8f5",
+    "url": "/hubble/static/js/12.bdeb1996.chunk.js"
+  },
+  {
+    "revision": "a10b5bf477afb1723664",
+    "url": "/hubble/static/js/13.a3013c8e.chunk.js"
+  },
+  {
+    "revision": "8fc37613579f61bd59a4",
+    "url": "/hubble/static/js/14.99f8f464.chunk.js"
+  },
+  {
+    "revision": "45bb1408482d2a5cfbd3",
+    "url": "/hubble/static/js/15.0cc5a79d.chunk.js"
+  },
+  {
+    "revision": "aa65d8d13737e6398448",
+    "url": "/hubble/static/js/2.b00a1a5d.chunk.js"
+  },
+  {
+    "revision": "75b7ab4a87d03928348f",
+    "url": "/hubble/static/js/5.572d1c23.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/hubble/static/js/5.572d1c23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b40a8b2f1138c4645253",
+    "url": "/hubble/static/js/6.44737690.chunk.js"
+  },
+  {
+    "revision": "407bdaf2907152d8fe12",
+    "url": "/hubble/static/js/7.f9f66a4d.chunk.js"
+  },
+  {
+    "revision": "8310857ad01c9d4a2e62",
+    "url": "/hubble/static/js/8.9ebb5b7b.chunk.js"
+  },
+  {
+    "revision": "fc39ecbfbf5df5cf57a5",
+    "url": "/hubble/static/js/9.fb87881f.chunk.js"
+  },
+  {
+    "revision": "2191bf6dfdbec0fe890d",
+    "url": "/hubble/static/js/main.e9770d64.chunk.js"
+  },
+  {
+    "revision": "6cf77b468223e7762dc9",
+    "url": "/hubble/static/js/runtime-main.a1381a42.js"
+  },
+  {
+    "revision": "d90b3bff9221bde91893bbc7a5083756",
+    "url": "/hubble/static/media/Hubble.d90b3bff.ttf"
+  }
+]);
